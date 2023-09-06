c3016598222ebf3810883d05baf5a278667b0fd1
Fauzan Aulia Hardy <fauzanhardy@gmail.com>
2023-09-06 21:36:08 +0700

    Project Finished

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Add a submit event listener to the form
     form.addEventListener('submit', function (event) {
         event.preventDefault(); // Prevent the default form submission behavior
-        
+
         // Get the values from form fields using their IDs
         const fullName = form.querySelector('#fname').value;
         const birthDate = form.querySelector('#birthday').value;
@@ -24,6 +24,21 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('#output-tanggal').textContent = birthDate;
         document.querySelector('#output-jenis-kelamin').textContent = gender;
         document.querySelector('#output-pesan').textContent = message;
+    });
+
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navbar = document.querySelector(".navbar");
+
+    // Toggle the menu when the hamburger icon is clicked
+    menuToggle.addEventListener("click", function () {
+        navbar.classList.toggle("active");
+    });
+
+    // Close the menu when clicking outside of it
+    document.addEventListener("click", function (event) {
+        if (!navbar.contains(event.target) && !menuToggle.contains(event.target)) {
+            navbar.classList.remove("active");
+        }
     });
 
 });
